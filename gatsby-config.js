3285f8491bbcc90f9c6fc4a77ba9ead0c7545384
Happytreat <cowfats@gmail.com>
2019-07-30 07:42:49 +0800

    Completed tutorial 8 - PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,28 @@ module.exports = {
     title: `Pandas Eating Lots`,
   },
   plugins: [
+    // PWA Web manifest
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // Add service workers
+    // runs in the background, deciding to serve network or cached
+    // content based on connectivity, allowing for a seamless,
+    // managed offline experience.
+    `gatsby-plugin-offline`,
+    // Add metadata to pages using React helmet
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
